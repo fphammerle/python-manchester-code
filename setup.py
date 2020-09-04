4f1a2774b34fa70b1d206996e316dff2fe6f3ef2
Fabian Peter Hammerle <fabian@hammerle.me>
2020-09-04 16:28:40 +0200

    added GPLv3+, setup.py & dev/pipeline env

---
+++ setup.py
@@ -0,0 +1,53 @@
+# manchester-code - Python library to encode & decode data as Manchester code
+#
+# Copyright (C) 2020 Fabian Peter Hammerle <fabian@hammerle.me>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import pathlib
+
+import setuptools
+
+_REPO_URL = "https://github.com/fphammerle/manchester-code"
+
+setuptools.setup(
+    name="manchester-code",
+    use_scm_version=True,
+    packages=setuptools.find_packages(),
+    description="Python library to encode & decode data as Manchester code",
+    long_description=pathlib.Path(__file__).parent.joinpath("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="Fabian Peter Hammerle",
+    author_email="fabian+python-manchester-code@hammerle.me",
+    url=_REPO_URL,
+    project_urls={"Changelog": _REPO_URL + "/blob/master/CHANGELOG.md"},
+    license="GPLv3+",
+    keywords=["phase encoding", "radio frequency", "telecommunication", "transmission"],
+    classifiers=[
+        # https://pypi.org/classifiers/
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX :: Linux",
+        # .github/workflows/python.yml
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Communications",
+    ],
+    install_requires=[],
+    setup_requires=["setuptools_scm"],
+    tests_require=["pytest"],
+)
