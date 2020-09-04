ad4592864c127579f5f4ff278da19fa90f43f84e
Fabian Peter Hammerle <fabian@hammerle.me>
2020-09-04 15:40:44 +0200

    added decode()

--- manchester_code.py
+++ manchester_code.py
@@ -3,18 +3,46 @@ import typing
 
 def encode(data: typing.Union[bytes, bytearray, typing.List[int]]) -> bytes:
     """
-    G. E. Thomas
+    G. E. Thomas convention
     """
-    encoded_data: typing.List[int] = []
+    manchester_code: typing.List[int] = []
     for byte in data:
-        encoded_data.extend((0, 0))
+        manchester_code.extend((0, 0))
         for bit_index in range(7, -1, -1):
             if byte & (0b1 << bit_index):
                 bits = 0b10
             else:
                 bits = 0b01
             if bit_index >= 4:
-                encoded_data[-2] |= bits << (bit_index * 2 - 8)
+                manchester_code[-2] |= bits << (bit_index * 2 - 8)
             else:
-                encoded_data[-1] |= bits << (bit_index * 2)
-    return bytes(encoded_data)
+                manchester_code[-1] |= bits << (bit_index * 2)
+    return bytes(manchester_code)
+
+
+def _decode_manchester_bit(manchester_bit: int) -> int:
+    if manchester_bit == 0b10:
+        return 1
+    if manchester_bit == 0b01:
+        return 0
+    raise ValueError("invalid bit 0b{:02b}".format(manchester_bit))
+
+
+def decode(manchester_code: typing.Union[bytes, bytearray, typing.List[int]]) -> bytes:
+    """
+    G. E. Thomas convention
+    """
+    data = bytearray()
+    for manchester_byte in zip(manchester_code[0::2], manchester_code[1::2]):
+        byte = 0
+        for bit_index in range(4):
+            byte |= (
+                _decode_manchester_bit((manchester_byte[1] >> (bit_index * 2)) & 0b11)
+                << bit_index
+            )
+            byte |= (
+                _decode_manchester_bit((manchester_byte[0] >> (bit_index * 2)) & 0b11)
+                << 4
+            ) << bit_index
+        data.append(byte)
+    return bytes(data)
--- test_code.py
+++ test_code.py
@@ -2,26 +2,32 @@ import pytest
 
 import manchester_code
 
-
-@pytest.mark.parametrize(
-    ("data", "encoded"),
+_DATA_MANCHESTER_CODE_MAPPING = (
+    ([0b00000000], [0b01010101, 0b01010101]),
+    ([0b11111111], [0b10101010, 0b10101010]),
+    ([0b01101001], [0b01101001, 0b10010110]),
+    ([0b11101000], [0b10101001, 0b10010101]),
+    ([0b01011010], [0b01100110, 0b10011001]),
     (
-        ([0b00000000], [0b01010101, 0b01010101]),
-        ([0b11111111], [0b10101010, 0b10101010]),
-        ([0b01101001], [0b01101001, 0b10010110]),
-        ([0b11101000], [0b10101001, 0b10010101]),
-        ([0b01011010], [0b01100110, 0b10011001]),
-        (
-            [0b11111111, 0b00000000, 0b01101001],
-            [0b10101010, 0b10101010, 0b01010101, 0b01010101, 0b01101001, 0b10010110],
-        ),
-        (
-            [0b01101001, 0b11111111, 0b00000000],
-            [0b01101001, 0b10010110, 0b10101010, 0b10101010, 0b01010101, 0b01010101],
-        ),
+        [0b11111111, 0b00000000, 0b01101001],
+        [0b10101010, 0b10101010, 0b01010101, 0b01010101, 0b01101001, 0b10010110],
+    ),
+    (
+        [0b01101001, 0b11111111, 0b00000000],
+        [0b01101001, 0b10010110, 0b10101010, 0b10101010, 0b01010101, 0b01010101],
     ),
 )
-def test_encode(data, encoded):
-    assert bytes(encoded) == manchester_code.encode(data)
-    assert bytes(encoded) == manchester_code.encode(bytes(data))
-    assert bytes(encoded) == manchester_code.encode(bytearray(data))
+
+
+@pytest.mark.parametrize(("data", "code"), _DATA_MANCHESTER_CODE_MAPPING)
+def test_encode(data, code):
+    assert bytes(code) == manchester_code.encode(data)
+    assert bytes(code) == manchester_code.encode(bytes(data))
+    assert bytes(code) == manchester_code.encode(bytearray(data))
+
+
+@pytest.mark.parametrize(("data", "code"), _DATA_MANCHESTER_CODE_MAPPING)
+def test_decode(data, code):
+    assert bytes(data) == manchester_code.decode(code)
+    assert bytes(data) == manchester_code.decode(bytes(code))
+    assert bytes(data) == manchester_code.decode(bytearray(code))
