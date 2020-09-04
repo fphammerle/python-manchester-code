4afef92ffe3321a3206ec36639ea3c04c282b19d
Fabian Peter Hammerle <fabian@hammerle.me>
2020-09-04 16:57:02 +0200

    added missing module docstring to make pylint happy

--- manchester_code.py
+++ manchester_code.py
@@ -15,6 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+"""
+Encode & decode data as Manchester code <https://en.wikipedia.org/wiki/Manchester_code>
+"""
+
 import typing
 
 
