b957424b6c3676df0f07294072cbb4b7904d2f48
Fabian Peter Hammerle <fabian@hammerle.me>
2023-07-29 07:31:33 +0200

    drop compatibility with python3.7 (reached end of life on 2023-06-27)
    https://web.archive.org/web/20230722054714/https://docs.python.org/3/whatsnew/3.8.html
    
    https://github.com/fphammerle/ical2vdir/commit/064bc9f99b0e6afdf0af08b7d146eb7cf032fd12

--- setup.py
+++ setup.py
@@ -41,13 +41,12 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: POSIX :: Linux",
         # .github/workflows/python.yml
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Topic :: Communications",
     ],
-    python_requires=">=3.7",  # python<3.7 untested
+    python_requires=">=3.8",  # python<3.8 untested
     install_requires=[],
     setup_requires=["setuptools_scm"],
     tests_require=["pytest"],
